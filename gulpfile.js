bdea03710d250bec9887c8f73a2b561be2e46936
Jose Angel Conchello <jose_a_conchello@hotmail.com>
2017-01-18 11:07:02 -0500

    Improved gulpfile.js + package.json

--- gulpfile.js
+++ gulpfile.js
@@ -7,14 +7,18 @@ var gulp = require('gulp');
 var gutil = require('gulp-util'),
     coffee = require('gulp-coffee');
 
+//  The 'src' method can take a file name or an array of file names.
+//  JavaScript array syntax: ['file1', 'file2', ..., 'fileN']
+//  File names can include wildcards e.g. 'components/coffee/*.coffee'
+var coffeeSources = ['components/coffee/tagline.coffee'];
+
 //  Create a task
 gulp.task('coffee', function ()   {// 'log' = task name (whatever we want)
 //  The function body is what we want 'gulp' to do
-//  The 'src' method can take a file name or an array of file names.
-//  JavaScript array syntax: ['file1', 'file2', ..., 'fileN']
-  gulp.src('components/coffee/tagline.coffee')
+  gulp.src(coffeeSources)
     .pipe( coffee({bare: true}) // Make a .js form the .coffee
       .on('error', gutil.log) ) // What to do in case of a coffee error
+                                // .log logs the error (to stdout?)
     .pipe( gulp.dest('components/scripts') ) //where to place result
 });
 
